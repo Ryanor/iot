1e27c6b3ec257d76e1600e89501b505a55cabe79
Unknown <gwuehrer@gmx.at>
2017-10-11 08:40:04 +0200

    Renamed file and added code from schuetzi

--- Portfolio/Exercise03/mqtt_subscriber.py
+++ Portfolio/Exercise03/mqtt_subscriber.py
@@ -1,13 +1,33 @@
-#!/usr/bin/env python
 import paho.mqtt.client as mqtt
+
+
+status = 0
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-    client.subscribe("test/#")
+    client.subscribe("test/temperature")
+
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
+    if msg.payload > 20 and status == 1:
+        off()
+        client.publish("test/switch", 0)
+    elif msg.payload < 20 and status == 0:
+        on()
+        client.publish("test/switch", 0)
+
+def off():
+    print("Turned off")
+    global status
+    status = 0
+
+def on():
+    print("Turned on")
+    global status
+    status = 1
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
